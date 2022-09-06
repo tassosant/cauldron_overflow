e32e8e01a04f02b4db3b3849d3b9bab2dad217fd
Tasos Antoniou <tassant@hotmail.com>
2022-09-06 13:14:33 +0300

    Chapter 4

---
+++ src/Controller/TestController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+class TestController
+{
+    public function test()
+    {
+
+        return new Response();
+    }
+}
