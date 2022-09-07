c0efe6c367ba0129efb1f083769090b1a57c1b6c
Tasos Antoniou <tassant@hotmail.com>
2022-09-07 16:44:48 +0300

    Chapter 10, Twig

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,11 +1,12 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-class QuestionController{
+class QuestionController extends AbstractController {
 
     
 /**
@@ -21,8 +22,17 @@ class QuestionController{
  */
     public function show($slug){
 
-        return new Response(sprintf(
-            ucwords(str_replace('-', ' ', $slug))
-        ));
+        $answers = [
+            'Make sure your cat is sitting purrrfectly still ?',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+
+
+
+        return $this->render('question/show.html.twig',[
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers,
+        ]);
     }
 }
